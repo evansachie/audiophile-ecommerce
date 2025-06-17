c338a99754723140603a784854cbaf12b14ab41f
Evans Acheampong <evansachie01@gmail.com>
2025-06-17 14:19:47 +0000

    refactor: update footer section

--- src/components/layout/Footer/Footer.tsx
+++ src/components/layout/Footer/Footer.tsx
@@ -5,6 +5,50 @@ import { TwitterIcon } from '../../icons/TwitterIcon';
 import { InstagramIcon } from '../../icons/InstagramIcon';
 import { Logo } from '../Header/Logo';
 
+const navItems = [
+  { to: '/', label: 'HOME' },
+  { to: '/headphones', label: 'HEADPHONES' },
+  { to: '/speakers', label: 'SPEAKERS' },
+  { to: '/earphones', label: 'EARPHONES' },
+];
+
+const SocialLinks = () => (
+  <div className="flex gap-4">
+    <a 
+      href="#" 
+      className="text-white hover:text-audiophile-orange transition-colors duration-200"
+      aria-label="Facebook"
+    >
+      <FacebookIcon className="w-6 h-6" />
+    </a>
+    <a 
+      href="#" 
+      className="text-white hover:text-audiophile-orange transition-colors duration-200"
+      aria-label="Twitter"
+    >
+      <TwitterIcon className="w-6 h-6" />
+    </a>
+    <a 
+      href="#" 
+      className="text-white hover:text-audiophile-orange transition-colors duration-200"
+      aria-label="Instagram"
+    >
+      <InstagramIcon className="w-6 h-6" />
+    </a>
+  </div>
+);
+
+const CompanyDescription = ({ className = '' }) => (
+  <p className={`text-body text-gray-400 leading-relaxed ${className}`}>
+    Audiophile is an all in one stop to fulfill your audio needs. We're a small team of music lovers and sound specialists who are devoted to helping you get the most out of personal audio. Come and visit our demo facility - we're open 7 days a week.
+  </p>
+);
+
+const CopyrightText = ({ className = '' }) => (
+  <p className={`text-body text-gray-500 ${className}`}>
+    Copyright 2021. All Rights Reserved
+  </p>
+);
 
 export const Footer = () => {
   return (
@@ -20,53 +64,26 @@ export const Footer = () => {
 
             <nav className="mb-12">
               <div className="flex flex-col gap-6">
-                <Link to="/" className="text-subtitle hover:text-audiophile-orange transition-colors duration-200">
-                  HOME
-                </Link>
-                <Link to="/headphones" className="text-subtitle hover:text-audiophile-orange transition-colors duration-200">
-                  HEADPHONES
-                </Link>
-                <Link to="/speakers" className="text-subtitle hover:text-audiophile-orange transition-colors duration-200">
-                  SPEAKERS
-                </Link>
-                <Link to="/earphones" className="text-subtitle hover:text-audiophile-orange transition-colors duration-200">
-                  EARPHONES
-                </Link>
+                {navItems.map((item) => (
+                  <Link 
+                    key={item.to} 
+                    to={item.to} 
+                    className="text-subtitle hover:text-audiophile-orange transition-colors duration-200"
+                  >
+                    {item.label}
+                  </Link>
+                ))}
               </div>
             </nav>
 
-            <p className="text-body text-gray-400 leading-relaxed mb-12">
-              Audiophile is an all in one stop to fulfill your audio needs. We're a small team of music lovers and sound specialists who are devoted to helping you get the most out of personal audio. Come and visit our demo facility - we're open 7 days a week.
-            </p>
+            <CompanyDescription className="mb-12" />
 
-            <div className="flex gap-4 justify-center mb-12">
-              <a 
-                href="#" 
-                className="text-white hover:text-audiophile-orange transition-colors duration-200"
-                aria-label="Facebook"
-              >
-                <FacebookIcon className="w-6 h-6" />
-              </a>
-              <a 
-                href="#" 
-                className="text-white hover:text-audiophile-orange transition-colors duration-200"
-                aria-label="Twitter"
-              >
-                <TwitterIcon className="w-6 h-6" />
-              </a>
-              <a 
-                href="#" 
-                className="text-white hover:text-audiophile-orange transition-colors duration-200"
-                aria-label="Instagram"
-              >
-                <InstagramIcon className="w-6 h-6" />
-              </a>
+            <div className="justify-center mb-12">
+              <SocialLinks />
             </div>
 
             {/* Copyright */}
-            <p className="text-body text-gray-500">
-              Copyright 2021. All Rights Reserved
-            </p>
+            <CopyrightText />
           </div>
 
           {/* Desktop Layout */}
@@ -75,60 +92,31 @@ export const Footer = () => {
             <div className="flex justify-between items-center mb-8">
               <Logo />
 
-            <nav>
+              <nav>
                 <div className="flex gap-8">
-                  <Link to="/" className="text-subtitle hover:text-audiophile-orange transition-colors duration-200">
-                    HOME
-                  </Link>
-                  <Link to="/headphones" className="text-subtitle hover:text-audiophile-orange transition-colors duration-200">
-                    HEADPHONES
-                  </Link>
-                  <Link to="/speakers" className="text-subtitle hover:text-audiophile-orange transition-colors duration-200">
-                    SPEAKERS
-                  </Link>
-                  <Link to="/earphones" className="text-subtitle hover:text-audiophile-orange transition-colors duration-200">
-                    EARPHONES
-                  </Link>
+                  {navItems.map((item) => (
+                    <Link 
+                      key={item.to} 
+                      to={item.to} 
+                      className="text-subtitle hover:text-audiophile-orange transition-colors duration-200"
+                    >
+                      {item.label}
+                    </Link>
+                  ))}
                 </div>
               </nav>
             </div>
             
             <div className="flex justify-between items-end">
               <div className="max-w-lg">
-                <p className="text-body text-gray-400 leading-relaxed">
-                  Audiophile is an all in one stop to fulfill your audio needs. We're a small team of music lovers and sound specialists who are devoted to helping you get the most out of personal audio. Come and visit our demo facility - we're open 7 days a week.
-                </p>
+                <CompanyDescription />
               </div>
 
-              <div className="flex gap-4">
-                <a 
-                  href="#" 
-                  className="text-white hover:text-audiophile-orange transition-colors duration-200"
-                  aria-label="Facebook"
-                >
-                  <FacebookIcon className="w-6 h-6" />
-                </a>
-                <a 
-                  href="#" 
-                  className="text-white hover:text-audiophile-orange transition-colors duration-200"
-                  aria-label="Twitter"
-                >
-                  <TwitterIcon className="w-6 h-6" />
-                </a>
-                <a 
-                  href="#" 
-                  className="text-white hover:text-audiophile-orange transition-colors duration-200"
-                  aria-label="Instagram"
-                >
-                  <InstagramIcon className="w-6 h-6" />
-                </a>
-              </div>
+              <SocialLinks />
             </div>
 
             <div className="mt-12">
-              <p className="text-body text-gray-500">
-                Copyright 2021. All Rights Reserved
-              </p>
+              <CopyrightText />
             </div>
           </div>
         </div>
